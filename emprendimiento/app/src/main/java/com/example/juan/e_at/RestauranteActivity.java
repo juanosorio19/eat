182c29c539d2f041bc2399952aba40bfd7eaa354
juanosorio19 <josorio19@utp.edu.co>
2016-12-10 01:35:02 -0500

    Added new views

--- emprendimiento/app/src/main/java/com/example/juan/e_at/RestauranteActivity.java
+++ emprendimiento/app/src/main/java/com/example/juan/e_at/RestauranteActivity.java
@@ -3,12 +3,11 @@ package com.example.juan.e_at;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RestauranteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
+        setContentView(R.layout.activity_restaurante);
     }
 }
